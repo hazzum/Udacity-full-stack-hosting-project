bce2cd039f688f9d6d49da8ab027a40b82ce24b8
hazzum <hazem.mix2012@gmail.com>
2022-11-01 04:55:01 +0200

    Update build.sh

--- udagram/udagram-api/bin/build.sh
+++ udagram/udagram-api/bin/build.sh
@@ -1,1 +1,2 @@
-npm install . && npm run clean && tsc & cp -rf .elasticbeanstalk www/.elasticbeanstalk && cp .npmrc www/.npmrc && cp package.json www/package.json && cd www && npx bestzip archive.zip * && cd ..
+npm install . && npm run clean && tsc
+cp -R .elasticbeanstalk www/.elasticbeanstalk/ && cp .npmrc www/.npmrc && cp package.json www/package.json && cd www && npx bestzip archive.zip * && cd ..
