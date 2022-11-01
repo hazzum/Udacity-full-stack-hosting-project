fe05201d832f32aefe3688fd9804dbe6eb9014e4
hazzum <hazem.mix2012@gmail.com>
2022-11-01 03:52:25 +0200

    new workflow

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+aws s3api create-bucket --bucket my-udagram-client --region us-east-1 
+aws s3 cp --recursive --acl public-read ./www s3://my-udagram-client/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-udagram-client/
