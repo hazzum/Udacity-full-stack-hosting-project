54f734e6c9e7d0f2a7774ab7d0aaa1eada8eb255
hazzum <hazem.mix2012@gmail.com>
2022-11-01 15:29:16 +0200

    Added screenshots, diagram

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,4 @@
 aws s3api create-bucket --bucket my-udagram-client --region us-east-1 
 aws s3 cp --recursive --acl public-read ./www s3://my-udagram-client/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-udagram-client/
+aws s3 website s3://my-udagram-client/ --index-document index.html
